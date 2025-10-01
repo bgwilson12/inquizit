bfbc45a25426968952f439ef864bd2da584fa328
Bret Wilson <bret@blaide.com>
2025-10-01 15:55:57 -0700

    configured cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className="flex flex-col gap-2 items-center w-full">
+				{children}
+			</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,35 @@
 import initialData from "../../data.json";
+import { Settings } from "lucide-react";
 
 function QuizItem({ item }: { item: { id: string; name: string } }) {
-	return <div className="bg-gray-500 text-9xl">{item.name}</div>;
+	return (
+		<div className="h-56 w-56 p-2 m-2 shadow-lg rounded-xl bg-gray-100">
+			<div>{item.name}</div>
+			<div className="flex justify-between ">
+				<p>Percent Correct</p>
+				<p>{90}%</p>
+			</div>
+			<div className="flex justify-between ">
+				<p>Attempts:</p>
+				<p>{4}</p>
+			</div>
+			<div className="flex justify-between ">
+				<p>Configure</p>
+				<Settings />
+			</div>
+		</div>
+	);
 }
 
 export default function Home() {
 	return (
-		<>
+		<div className="w-3xl border-2">
 			<header>
 				<h1>Home</h1>
 			</header>
 
-			<main className="bg-gray-600">
-				<div className="bg-gray-500">
+			<main className="">
+				<div className="flex">
 					{initialData.map((item) => {
 						return <QuizItem key={item.id} item={item} />;
 					})}
@@ -22,6 +39,6 @@ export default function Home() {
 			<footer>
 				<p>&copy; 2025 Example Company</p>
 			</footer>
-		</>
+		</div>
 	);
 }
